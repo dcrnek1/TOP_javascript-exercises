a9e37a7e1de683375599a6ca5cf1f2a0f48ec50b
dcrnek1 <dario.crnek@fina.hr>
2024-04-15 14:35:58 +0200

    completed tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, numOfTimes) {
+    let repeatedStr = '';
 
+    for (let i = 0; i < numOfTimes; i++) {
+        repeatedStr += str.toString();
+    }
+
+    return numOfTimes >= 0 ? repeatedStr : 'ERROR';
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number' || Math.min(a, b) < 0) return "ERROR";
 
+    let sum = 0;
+    for (i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
