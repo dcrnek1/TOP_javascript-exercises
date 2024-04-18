529c7ba10d9132a929984ce209488c133c82aaf4
dcrnek1 <dario.crnek@gmail.com>
2024-04-18 15:16:38 +0200

    Calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.length > 0 ? numArray.reduce((total, currentNumber) => total + currentNumber) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((total, currentNum) => total * currentNum);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = a;
+  for (let i = 1; i < b; i++) {
+    result *= a;
+  }
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	return num > 0 ? num * factorial(num - 1) : 1;
 };
 
 // Do not edit below this line
