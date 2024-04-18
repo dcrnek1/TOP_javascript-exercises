cc95a7c49d36c87454e3bf1e1f44b89ceba3f121
dcrnek1 <dario.crnek@gmail.com>
2024-04-18 16:00:57 +0200

    Fibbonaci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (typeof num !== 'number') {
+        num = +num;
+    } 
+    if (num < 0) {
+        return "OOPS";
+    } else if (num == 0) {
+        return 0;
+    }
+
+    numStore = {
+        prev: 0,
+        current: 1,
+        currentTemp: 1,
+    }
+
+    let fibbonaciArray = [];
+
+    for (let i = 0; i < num; i++) {
+        numStore.currentTemp = numStore.current;
+        numStore.current = numStore.current + numStore.prev;
+        numStore.prev = numStore.currentTemp;
+        fibbonaciArray.push(numStore.prev);
+    }
+
+    return fibbonaciArray[fibbonaciArray.length-1];
+    
 };
 
 // Do not edit below this line
