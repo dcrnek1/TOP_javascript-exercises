877b6a9cedc117dcc1e992f9e3a74c58be5b9ef0
dcrnek1 <dario.crnek@gmail.com>
2024-04-18 15:36:15 +0200

    Palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const punctuation = ',.! ?:;';
 
+const palindromes = function (input) {
+    let notReversed = input.split('').filter((current) => !punctuation.includes(current)).reduce((w, c) => w += c).toLowerCase();
+    let reversed = input.split('').filter((current) => !punctuation.includes(current)).reduce((whole, current) => current += whole).toLowerCase();
+    
+    return notReversed == reversed;
 };
 
 // Do not edit below this line
